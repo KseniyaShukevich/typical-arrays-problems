6f2c57f11d44091e279323b57ccccda9e7238319
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-09-29 00:22:45 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
+    if (array && array.length !== 0) {
+        let min = array[0];
+
+        for (let value of array) {
+            if (value < min) {
+                min = value;
+            }
+        }
+
+        return min;
+    }
+
   return 0;
 }
 
 exports.max = function max (array) {
+    if (array && array.length !== 0) {
+        let max = array[0];
+
+        for (let value of array) {
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        return max;
+    }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if (array && array.length !== 0) {
+        let sum = 0;
+
+        for (let value of array) {
+            sum += value;
+        }
+
+        return sum / array.length;
+    }
+
   return 0;
 }
